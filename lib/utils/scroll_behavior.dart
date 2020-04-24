78d89b4b2b3eddd0f93cabfcc56ab00600b05874
Ray Li <ray.li.verified@gmail.com>
2020-04-24 08:54:11 -0500

    Create Clamping Scroll Behavior
    *Add Clamping Scroll Behavior.

--- lib/utils/scroll_behavior.dart
+++ lib/utils/scroll_behavior.dart
@@ -32,3 +32,36 @@ class BouncingScrollWrapper extends StatelessWidget {
     );
   }
 }
+
+class ClampingScrollBehavior extends ScrollBehavior {
+  // Disable overscroll glow.
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+
+  // Set physics to bouncing.
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return ClampingScrollPhysics();
+  }
+}
+
+class ClampingScrollWrapper extends StatelessWidget {
+  final Widget child;
+
+  const ClampingScrollWrapper({Key key, this.child}) : super(key: key);
+
+  static Widget builder(BuildContext context, Widget child) {
+    return ClampingScrollWrapper(child: child);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScrollConfiguration(
+      behavior: ClampingScrollBehavior(),
+      child: child,
+    );
+  }
+}
