2c18755e5e1b5cbcc8f44431df35f2c6371c0166
Ray Li <ray.li.verified@gmail.com>
2021-10-30 16:52:13 -0500

    Update ShortestSide Documentation

--- lib/responsive_wrapper.dart
+++ lib/responsive_wrapper.dart
@@ -75,8 +75,26 @@ class ResponsiveWrapper extends StatefulWidget {
   final bool defaultScale;
   final double defaultScaleFactor;
 
-  /// Useful if you want the scaling/resizing to be calculated based on the shortest
-  /// side of the screen, instead of using just width
+  /// Calculate responsiveness based on the shortest
+  /// side of the screen, instead of the actual
+  /// landscape orientation.
+  ///
+  /// This is useful for apps that want to avoid
+  /// scrolling screens and distribute their content
+  /// based on width/height regardless of orientation.
+  /// Size units can remain the same when the phone
+  /// is in landscape mode or portrait mode.
+  /// The developer needs only change a few widgets'
+  /// hard-coded size depending on the orientation.
+  /// The rest of the widgets maintain their size but
+  /// change the way they are displayed.
+  ///
+  /// `useShortestSide` can be used in conjunction with
+  /// [breakpointsLandscape] for additional configurability.
+  /// Landscape breakpoints will activate when the
+  /// physical device is in landscape mode but base
+  /// calculations on the shortest side instead of
+  /// the actual landscape width.
   final bool useShortestSide;
 
   /// Landscape minWidth value. Defaults to [minWidth] if not set.
@@ -237,10 +255,10 @@ class _ResponsiveWrapperState extends State<ResponsiveWrapper>
   /// Get screen width calculation.
   double screenWidth = 0;
   double getScreenWidth() {
-    double _size = useShortestSide
+    double widthCalc = useShortestSide
         ? (windowWidth < windowHeight ? windowWidth : windowHeight)
         : windowWidth;
-    activeBreakpointSegment = getActiveBreakpointSegment(_size);
+    activeBreakpointSegment = getActiveBreakpointSegment(widthCalc);
     // Special 0 width condition.
     if (activeBreakpointSegment.responsiveBreakpoint.breakpoint == 0) return 0;
     // Check if screenWidth exceeds maxWidth.
