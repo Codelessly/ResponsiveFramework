76417c39bfdfc422bcbd71f513a0c3cac9d21c5e
Ray Li <ray.li.verified@gmail.com>
2023-02-19 18:36:20 -0600

    Breakpoint Model Creation

---
+++ lib/breakpoint.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Breakpoint {
+  final double start;
+  final double end;
+  final String? name;
+  final dynamic? data;
+
+  @visibleForTesting
+  const Breakpoint(
+      {required this.start,
+        required this.end,
+        this.name, this.data});
+
+  Breakpoint copyWith({
+    double? start,
+    double? end,
+    String? name,
+    dynamic data,
+  }) =>
+      Breakpoint(
+        start: start ?? this.start,
+        end: end ?? this.end,
+        name: name ?? this.name,
+        data: data ?? this.data,
+      );
+
+  @override
+  String toString() =>
+      'Breakpoint(start: $start, end: $end, name: $name)';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Breakpoint &&
+              runtimeType == other.runtimeType &&
+              start  == other.start  &&
+              end  == other.end  &&
+              name == other.name;
+
+  @override
+  int get hashCode =>
+      start.hashCode *
+          end.hashCode *
+          name.hashCode;
+}
