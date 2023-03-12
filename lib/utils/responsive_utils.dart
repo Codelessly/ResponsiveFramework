1cc6d110bcb575497d20e4d910198a0de94f912a
Ray Li <ray.li.verified@gmail.com>
2023-03-12 13:58:24 -0500

    Responsive Utils Rewrite

--- lib/utils/responsive_utils.dart
+++ lib/utils/responsive_utils.dart
@@ -1,93 +1,57 @@
 import 'package:flutter/foundation.dart';
 
 import '../breakpoint.dart';
-import '../responsive_wrapper.dart';
 
 /// Util functions for the Responsive Framework.
 class ResponsiveUtils {
-  /// [ResponsiveBreakpointBehavior] order.
-  ///
-  /// For breakpoints with the same value, ordering
-  /// controls proper breakpoint behavior resolution.
-  /// Preserve input order for breakpoints..
-  /// Tags are always ranked last because they are
-  /// inert.
-  static Map<ResponsiveBreakpointBehavior, int> breakpointCompartorList = {
-    ResponsiveBreakpointBehavior.AUTOSCALEDOWN: 0,
-    ResponsiveBreakpointBehavior.RESIZE: 0,
-    ResponsiveBreakpointBehavior.AUTOSCALE: 0,
-    ResponsiveBreakpointBehavior.TAG: 1,
-  };
-
-  /// Comparator function to order [ResponsiveBreakpoint]s.
-  ///
-  /// Order breakpoints from smallest to largest based
-  /// on breakpoint value.
-  /// When breakpoint values are equal, map
-  /// [ResponsiveBreakpointBehavior] to their
-  /// ordering value in [breakpointCompartorList]
-  /// and compare.
-  static int responsiveBreakpointComparator(
-      ResponsiveBreakpoint a, ResponsiveBreakpoint b) {
-    // If breakpoints are equal, return in comparator order.
-    if (a.breakpoint == b.breakpoint) {
-      return breakpointCompartorList[a.behavior]!
-          .compareTo(breakpointCompartorList[b.behavior]!);
-    }
-
-    // Breakpoints are not equal can be compared directly.
-    return a.breakpoint.compareTo(b.breakpoint);
-  }
-
+  /// Comparator function to order [Breakpoint]s from small to large by start sections.
   static int breakpointComparator(Breakpoint a, Breakpoint b) {
     return a.start.compareTo(b.start);
   }
 
-  /// Print a visual view of [breakpointSegments]
+  /// Print a visual view of [breakpoints]
   /// for debugging purposes.
-  static String debugLogResponsiveBreakpointSegments(
-      List<ResponsiveBreakpointSegment> responsiveBreakpointSegments) {
+  static String debugLogBreakpoints(List<Breakpoint>? breakpoints) {
+    if (breakpoints == null || breakpoints.isEmpty) return '| Empty |';
+    List<Breakpoint> breakpointsHolder = List.from(breakpoints);
+    breakpointsHolder.sort(breakpointComparator);
+
     var stringBuffer = StringBuffer();
-    stringBuffer.write('|');
-    for (int i = 0; i < responsiveBreakpointSegments.length; i++) {
+    stringBuffer.write('| ');
+    for (int i = 0; i < breakpointsHolder.length; i++) {
       // Convenience variable.
-      ResponsiveBreakpointSegment segment = responsiveBreakpointSegments[i];
-      stringBuffer.write(segment.breakpoint.round());
+      Breakpoint breakpoint = breakpointsHolder[i];
+      stringBuffer.write(breakpoint.start);
+      stringBuffer.write(' ----- ');
       List<dynamic> attributes = [];
-      String? name = segment.responsiveBreakpoint.name;
+      String? name = breakpoint.name;
       if (name != null) attributes.add(name);
-      double scaleFactor = segment.responsiveBreakpoint.scaleFactor;
-      if (scaleFactor != 1) attributes.add(scaleFactor);
       if (attributes.isNotEmpty) {
         stringBuffer.write('(');
         for (int i = 0; i < attributes.length; i++) {
           stringBuffer.write(attributes[i]);
-          if (i != attributes.length - 1) stringBuffer.write('|');
+          if (i != attributes.length - 1) stringBuffer.write(',');
         }
         stringBuffer.write(')');
+        stringBuffer.write(' ----- ');
       }
-      stringBuffer.write(' ----- ');
-      if (segment.segmentType == ResponsiveBreakpointBehavior.AUTOSCALEDOWN &&
-          segment.breakpoint < segment.responsiveBreakpoint.breakpoint) {
-        stringBuffer.write(
-            '${describeEnum(segment.segmentType)} from ${segment.responsiveBreakpoint.breakpoint.round()}');
+      if (breakpoint.end == double.infinity) {
+        stringBuffer.write('∞');
       } else {
-        stringBuffer.write(describeEnum(segment.responsiveBreakpoint.behavior));
+        stringBuffer.write(breakpoint.end);
       }
-      if (i != responsiveBreakpointSegments.length - 1) {
+      if (i != breakpoints.length - 1) {
         stringBuffer.write(' ----- ');
       }
     }
-    stringBuffer.write(' ----- ∞ |');
+    stringBuffer.write(' |');
     debugPrint(stringBuffer.toString());
     return stringBuffer.toString();
   }
-
-  static String debugLogBreakpoints(List<Breakpoint>? breakpoints) {
-    return '';
-  }
 }
 
+/// A superset of [TargetPlatform] that includes [web] as an enum option.
+/// Flutter's TargetPlatform does not include web as a platform option.
 enum ResponsiveTargetPlatform {
   android,
   fuchsia,
@@ -98,6 +62,7 @@ enum ResponsiveTargetPlatform {
   web,
 }
 
+/// A helper extension to convert [TargetPlatform] to [ResponsiveTargetPlatform].
 extension TargetPlatformExtension on TargetPlatform {
   ResponsiveTargetPlatform get responsiveTargetPlatform {
     switch (this) {
