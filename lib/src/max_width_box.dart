557f3ca2641e56dd69bc87f6cf2bfb75fcfd7c26
Ray Li <ray.li.verified@gmail.com>
2024-08-25 18:03:59 -0500

    Create New MaxWidthBox
    - Remove Stack implementation, use ConstrainedBox directly.

--- lib/src/max_width_box.dart
+++ lib/src/max_width_box.dart
@@ -1,110 +1,49 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class MaxWidthBox extends StatefulWidget {
+class MaxWidthBox extends StatelessWidget {
   final double? maxWidth;
+  final Widget child;
 
-  /// Control the internal Stack alignment. This widget
-  /// uses a Stack to set the widget to max width on top of
-  /// a background.
+  /// Control child alignment.
   /// Defaults to [Alignment.topCenter] because app
   /// content is usually top aligned.
   final AlignmentGeometry alignment;
-  final Widget child;
-  final Widget? background;
+  final EdgeInsets? padding;
+  final Color? backgroundColor;
 
   const MaxWidthBox(
       {super.key,
       required this.maxWidth,
       required this.child,
-      this.background,
-      this.alignment = Alignment.topCenter});
-
-  @override
-  State<MaxWidthBox> createState() => _MaxWidthBoxState();
-}
-
-class _MaxWidthBoxState extends State<MaxWidthBox> {
-  final _scrollController = ScrollController();
+      this.alignment = Alignment.topCenter,
+      this.padding,
+      this.backgroundColor});
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQuery = MediaQuery.of(context);
 
-    if (widget.maxWidth != null) {
-      if (mediaQuery.size.width > widget.maxWidth!) {
+    if (maxWidth != null) {
+      if (mediaQuery.size.width > maxWidth!) {
         mediaQuery = mediaQuery.copyWith(
-            size: Size(widget.maxWidth!, mediaQuery.size.height));
+            size: Size(maxWidth! - (padding?.horizontal ?? 0),
+                mediaQuery.size.height - (padding?.vertical ?? 0)));
       }
     }
 
-    return Stack(
-      alignment: widget.alignment,
-      children: [
-        Scrollbar(
-          controller: _scrollController,
-          trackVisibility: true,
-          thumbVisibility: true,
-          child: Listener(
-            onPointerSignal: (pointerSignal) {
-              if (pointerSignal is PointerScrollEvent) {
-                final newOffset =
-                    _scrollController.offset + pointerSignal.scrollDelta.dy;
-
-                if (newOffset < _scrollController.position.minScrollExtent) {
-                  _scrollController
-                      .jumpTo(_scrollController.position.minScrollExtent);
-                } else if (newOffset >=
-                        _scrollController.position.minScrollExtent &&
-                    newOffset <= _scrollController.position.maxScrollExtent) {
-                  _scrollController.jumpTo(newOffset);
-                } else if (newOffset >
-                    _scrollController.position.maxScrollExtent) {
-                  _scrollController
-                      .jumpTo(_scrollController.position.maxScrollExtent);
-                }
-              }
-            },
-            child: widget.background ??
-                Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.transparent),
+    return Align(
+      alignment: alignment,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: maxWidth ?? double.infinity),
+        child: Container(
+          color: backgroundColor,
+          padding: padding,
+          child: MediaQuery(
+            data: mediaQuery,
+            child: child,
           ),
         ),
-        MediaQuery(
-          data: mediaQuery,
-          child: SizedBox(
-            width: widget.maxWidth,
-            child: PrimaryScrollController(
-              controller: _scrollController,
-              child: widget.child,
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
 }
-
-// NotificationListener<ScrollNotification>(
-//   onNotification: (scrollNotification) {
-//     // Forward the scroll notification to the child
-//     if (scrollNotification is ScrollUpdateNotification) {
-//       _scrollController.jumpTo(_scrollController.offset +
-//           scrollNotification.scrollDelta!.toDouble());
-//     }
-//     return false; // Don't consume the notification
-//   },
-//   child: widget.background ??
-//       Container(
-//           width: double.infinity,
-//           height: double.infinity,
-//           color: Colors.transparent),
-// ),
